9e7c328b387de696b5fb84c310f8d3068d7e07d9
Peter Turner <syntax.valid@gmail.com>
2026-04-06 16:08:26 +0100

    Added homepage URLs

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -9,6 +9,7 @@ function TopBar({ onAboutClick }: Props) {
     <div className={styles.bar}>
       <h1 className={styles.title}>Pander</h1>
       <div className={styles.links}>
+        <a className={styles.link} href="https://storage.googleapis.com/santyx-landing/turkey/index.html">Home</a>
         <button className={styles.link} onClick={onAboutClick}>About</button>
         <a className={styles.link} href="https://github.com/Syntax753/pander/issues" target="_blank" rel="noreferrer">Support</a>
       </div>
